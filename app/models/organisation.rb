03f09726fa613194596c486860f1c939ce4ee718
cynthia <yinnycfu@gmail.com>
2021-11-28 22:28:25 +0000

    feat: org model validates hourly rate to be 2dp and valid

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,5 +3,5 @@ class Organisation < ApplicationRecord
   has_many :users, :through => :memberships
 
   validates :name, presence: true, uniqueness: true
-  validates :hourly_rate, presence: true
+  validates :hourly_rate, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000 }
 end
--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -1,17 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Organisation, type: :model do
+  let(:organisation) { described_class.new(name: 'Test Name', hourly_rate: 11.00) }
+
   it { is_expected.to validate_presence_of(:name) }
 
   it { is_expected.to validate_presence_of(:hourly_rate) }
 
   it "has unique organisation names" do
-    organisation = described_class.new(name: 'Test Name', hourly_rate: 11.00)
     organisation.save
     duplicate_org = described_class.new(name: 'Test Name', hourly_rate: 11.00)
     expect(duplicate_org).not_to be_valid
   end
 
-  
+  it "creates a valid organisation object" do
+    expect(organisation).to be_valid
+    expect(organisation.name).to eq 'Test Name'
+    expect(organisation.hourly_rate).to eq 11.00
+  end
+
+  it "should accept valid hourly rates" do
+    valid_rates = [11.00, 11, 0.01, 999.99, "11", "11.11"]
+
+    valid_rates.each do |valid_rate|
+      organisation.hourly_rate = valid_rate
+      expect(organisation).to be_valid
+    end
+  end
+
+  it "shouldn't accept invalid hourly rates" do
+    invalid_rates = [0.001, "notarate"]
+
+    invalid_rates.each do |invalid_rate|
+      organisation.hourly_rate = invalid_rate
+      expect(organisation).not_to be_valid
+    end
+  end
 
 end
