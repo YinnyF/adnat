907738969299224fe6753736a9f776c483ab56a2
cynthia <yinnycfu@gmail.com>
2021-11-29 21:53:50 +0000

    fix: day and month bug - missing leading 0

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -26,7 +26,10 @@ class ShiftsController < ApplicationController
   # POST /shifts or /shifts.json
   def create
     # TODO can we refactor this to the model?
-    date_string = shift_params["date(1i)"] + shift_params["date(2i)"] + shift_params["date(3i)"]
+    day_string = "%02i" % shift_params["date(1i)"]
+    month_string = "%02i" % shift_params["date(2i)"]
+
+    date_string = day_string + month_string + shift_params["date(3i)"]
     start_time_string = shift_params["start(4i)"] + ":" + shift_params["start(5i)"]
     finish_time_string = shift_params["finish(4i)"] + ":" + shift_params["finish(5i)"]
     required_dt_format = "%Y%m%dT%H:%M"
